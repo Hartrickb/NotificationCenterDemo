14c5e1c84a2f34b2ddf3f8f8ac364ff19a17bad9
Hartrickb <Hartrickb@users.noreply.github.com>
2017-05-01 10:26:35 -0400

    create: demo app that lets you push a button to trigger a notification

--- NotificationCenterDemo/ViewController.swift
+++ NotificationCenterDemo/ViewController.swift
@@ -13,13 +13,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Register to listen for a notification
+        NotificationCenter.default.addObserver(self, selector: #selector(dataDownloaded(n:)), name: NSNotification.Name.init("DataDownloaded"), object: nil)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func dataDownloaded(n: NSNotification) {
+        print("triggered")
+    }
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        NotificationCenter.default.post(name: NSNotification.Name.init("DataDownloaded"), object: nil)
+        
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
